482b1b7ca2255a6efaa656ce2b668b08079c202e
kavimaxx <kavilakshan81@gmail.com>
2025-04-17 00:34:00 +0530

    Build webb app

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, redirect, request
+from helper import preprocessing, vectorizer, get_prediction
+
+app = Flask(__name__)
+
+# Make these global so we can update them
+data = {}
+reviews = []
+positive = 0
+negative = 0
+
+@app.route("/", methods=["GET"])
+def index():
+    data['reviews'] = reviews
+    data['positive'] = positive
+    data['negative'] = negative
+    return render_template('index.html', data=data)
+
+@app.route("/", methods=["POST"])
+def my_post():
+    global positive, negative  # Make sure we update the global counts
+    text = request.form['text']
+    preprocessed_text = preprocessing(text)
+    vectorized_text = vectorizer(preprocessed_text)
+    prediction = get_prediction(vectorized_text)
+
+    if prediction == 'negative':
+        negative += 1
+    else:
+        positive += 1
+
+    reviews.insert(0, text)
+    return redirect("/")
+    
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
